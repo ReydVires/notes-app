1fade914dae40ce4230069229931c53953541a5f
Ahmad Arsyel <arsyel@agate.id>
2021-01-19 11:53:29 +0700

    adding grid style

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,42 @@
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
-import { Button, Card } from "semantic-ui-react";
+import { Button, Card, Grid } from "semantic-ui-react";
 
-const Index = ({ notes }) => (
-	<div className="notes-container">
-		<h1>Notes</h1>
-		<div className="grid wrapper">
-			{notes.map((note) => (
-				<div key={note._id}>
-					<Card>
-						<Card.Content>
-							<Card.Header>
-								<Link href={`/${note._id}`}>
-									<a>{note.title}</a>
-								</Link>
-							</Card.Header>
-						</Card.Content>
-						<Card.Content extra>
-							<Link href={`/${note._id}`}>
-								<Button primary>View</Button>
-							</Link>
-							<Link href={`/${note._id}/edit`}>
-								<Button primary>Edit</Button>
-							</Link>
-						</Card.Content>
-					</Card>
-				</div>
-			))}
+const Index = ({ notes }) => {
+	const col = 3;
+	return (
+		<div className="notes-container">
+			<h1>Notes</h1>
+			<div className="grid">
+				<Grid columns={col} divided centered>
+				{notes.map((note) => (
+					<div key={note._id}>
+						<Card>
+							<Card.Content>
+								<Card.Header>
+									<Link href={`/${note._id}`}>
+										<a>{note.title}</a>
+									</Link>
+								</Card.Header>
+							</Card.Content>
+							<Card.Content extra>
+								<div className='ui two buttons'>
+									<Link href={`/${note._id}`}>
+										<Button basic color="blue">View</Button>
+									</Link>
+									<Link href={`/${note._id}/edit`}>
+										<Button basic color="yellow">Edit</Button>
+									</Link>
+								</div>
+							</Card.Content>
+						</Card>
+					</div>
+				))}
+				</Grid>
+			</div>
 		</div>
-	</div>
-);
+	);
+}
 
 // Rendering first on server-side
 Index.getInitialProps = async () => {
